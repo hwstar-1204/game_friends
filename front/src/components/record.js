e1fbeb2777942c8360180a452fbe005bfcd4d235
fullmoon1219 <ad001219@naver.com>
2024-11-25 17:36:33 +0900

    전적 보기 페이지 스타일 2차 완성

--- front/src/components/record.js
+++ front/src/components/record.js
@@ -13,6 +13,7 @@ function RecordPage() {
   useEffect(() => {
     setRecordData({
       friendName,
+      
       // 솔로랭크 티어 및 lp 포인트
       rankGames: [
         { season: 'S2024 S2', tier: 'Emerald 4', lp: 43 },
@@ -33,22 +34,130 @@ function RecordPage() {
           outcome: '승리', // 승리 or 패배 or 무승부
           matchTimeAgo: '22분 전', // 시간
           kda: '5 / 5 / 28', // KDA
+          champImg: '/img/user.png', // 챔피언 이미지
           champ: '챔피언 1', // 챔피언 이름
           lane: '4th', // 기여도
           cs: '17', // cs 개수
           avgCs: '3.4', // 분당 cs
-          participants: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'], // 팀원 이름
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'], // 레드팀
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'], // 블루팀
         },
         {
           type: '무작위 총력전',
           outcome: '패배',
-          matchTimeAgo: '43분 전',
-          kda: '13 / 8 / 9',
-          champ: '챔피언 2',
-          lane: '2nd',
-          cs: '14',
-          avgCs: '3.8',
-          participants: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
+        },
+        {
+          type: '무작위 총력전',
+          outcome: '패배',
+          matchTimeAgo: '22분 전',
+          kda: '5 / 5 / 28',
+          champImg: '/img/user.png',
+          champ: '챔피언 1',
+          lane: '4th',
+          cs: '17',
+          avgCs: '3.4',
+          participantsRed: ['플레이어 1', '플레이어 2', '플레이어 3', '플레이어 4'],
+          participantsBlue: ['플레이어 5', '플레이어 6', '플레이어 7', '플레이어 8'],
         },
       ],
     });
@@ -57,50 +166,91 @@ function RecordPage() {
   return (
     <Template friendsData={friendsData}>
         <div className="record-page">
-            <div className="record-left">
-                <div className="rank-games">
-                    <h3>개인/2인 랭크 게임</h3>
-                    <ul>
-                        {recordData?.rankGames?.map((game, index) => (
-                        <li key={index}>
-                            <span>{game.season}</span>
-                            <span>{game.tier}</span>
-                            <span>{game.lp} LP</span>
-                        </li>
-                        ))}
-                    </ul>
-                </div>
+          <div className="record-left">
+            <div className="record-player-info">
+              <div
+                className="record-player-profile"
+                style={{ 
+                  backgroundImage: `url(${ '/img/user.png'})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+              ></div>
+              
+              <h3>{friendName}</h3>
 
-                <div className="free-games">
-                    <h3>자유 랭크 게임</h3>
-                    <ul>
-                        {recordData?.freeGames?.map((game, index) => (
-                        <li key={index}>
-                            <span>{game.season}</span>
-                            <span>{game.tier}</span>
-                            <span>{game.lp} LP</span>
-                        </li>
-                        ))}
-                    </ul>
-                </div>
             </div>
 
-            <div className="record-right">
+            <div className="rank-games">
+              <h3>개인/2인 랭크 게임</h3>
+              <ul>
+                  {recordData?.rankGames?.map((game, index) => (
+                  <li key={index}>
+                      <span>{game.season}</span>
+                      <span>{game.tier}</span>
+                      <span>{game.lp} LP</span>
+                  </li>
+                  ))}
+                </ul>
+            </div>
 
-            
-                <div className="recent-games">
-                    <h3>최근 게임</h3>
-                    {recordData?.recentGames?.map((game, index) => (
-                        <div key={index} className={`game ${game.outcome === '승리' ? 'win' : 'lose'}`}>
+            <div className="free-games">
+              <h3>자유 랭크 게임</h3>
+              <ul>
+                {recordData?.freeGames?.map((game, index) => (
+                  <li key={index}>
+                    <span>{game.season}</span>
+                    <span>{game.tier}</span>
+                    <span>{game.lp} LP</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="record-right">
+
+          
+            <div className="recent-games">
+              <h3>최근 게임</h3>
+                <div className="recent-games-log" style={{ overflowY: 'auto', maxHeight: '700px' }}>
+                  {recordData?.recentGames?.map((game, index) => (
+                    <div key={index} className={`game ${game.outcome === '승리' ? 'win' : 'lose'}`}>
+                      <div className="recent-games-log-left"> 
                         <p>{game.type}</p>
-                        <p>{game.champ} - {game.kda}</p>
                         <p>{game.outcome} ({game.matchTimeAgo})</p>
+
+                      </div>
+
+                      <div className="recent-games-log-center"> 
+                        <div
+                          className="recent-champ-img"
+                          style={{ 
+                            backgroundImage: `url(${ '/img/user.png'})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                          }}
+                        ></div>
+                        <p>{game.champ} - {game.kda}</p>
                         <p>CS: {game.cs} ({game.avgCs})</p>
-                        <p>참여자: {game.participants.join(', ')}</p>
-                        </div>
-                    ))}
+                      </div>
+                      
+                      <div className="recent-games-log-right"> 
+                        <div className="recent-games-log-right-red"> 
+                          <p>{game.participantsRed.join(', ')}</p>
+                        </div> 
+
+                        <div className="recent-games-log-right-blue"> 
+                          <p>{game.participantsBlue.join(', ')}</p>
+                        </div> 
+
+                      </div>
+                    </div>
+                  ))}
+
                 </div>
             </div>
+          </div>
         </div>
     </Template>
   );
