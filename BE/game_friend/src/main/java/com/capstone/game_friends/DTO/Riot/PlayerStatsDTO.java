e11f188d1dcbc4d296a85a7859d43f65a8b49825
junho767 <junho767@naver.com>
2024-12-03 17:37:08 +0900

    refactor: 매치 정보 반환 수정
    아이템, 챔피언 초상화의 이미지의 URL을 반환하게끔 변경

--- BE/game_friend/src/main/java/com/capstone/game_friends/DTO/Riot/PlayerStatsDTO.java
+++ BE/game_friend/src/main/java/com/capstone/game_friends/DTO/Riot/PlayerStatsDTO.java
@@ -1,5 +1,6 @@
 package com.capstone.game_friends.DTO.Riot;
 
+import com.capstone.game_friends.Constant.RiotConstant;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,14 +9,14 @@ import lombok.Setter;
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true) // 정의되지 않은 필드를 무시
 public class PlayerStatsDTO {
-    // 아이템 리스트
-    private final int item0;
-    private final int item1;
-    private final int item2;
-    private final int item3;
-    private final int item4;
-    private final int item5;
-    private final int item6;
+    // 아이템 리스트 이미지 Url 을 반환
+    private final String item0;
+    private final String item1;
+    private final String item2;
+    private final String item3;
+    private final String item4;
+    private final String item5;
+    private final String item6;
 
     // KDA 관련
     private final int deaths;
@@ -35,6 +36,7 @@ public class PlayerStatsDTO {
     private final int championId;
     private final String championName;
     private final String teamPosition;
+    private final String championImageUrl;
 
     // 마법 데미지 관련
     private final int magicDamageDealt;
@@ -46,9 +48,9 @@ public class PlayerStatsDTO {
     private final int physicalDamageDealtToChampions;
     private final int physicalDamageTaken;
 
-    // 소환사 주문 id
-    private final int summoner1Id;
-    private final int summoner2Id;
+    // 소환사 주문 이미지 Url 을 반환
+    private final String summonerSpell1Url;
+    private final String summonerSpell2Url;
 
 
     // 전체 딜량
@@ -79,20 +81,19 @@ public class PlayerStatsDTO {
     private final int wardsKilled;
     private final int wardsPlaced;
 
-    public PlayerStatsDTO(ParticipantDTO participantDTO) {
-        this.item0 = participantDTO.getItem0();
-        this.item1 = participantDTO.getItem1();
-        this.item2 = participantDTO.getItem2();
-        this.item3 = participantDTO.getItem3();
-        this.item4 = participantDTO.getItem4();
-        this.item5 = participantDTO.getItem5();
-        this.item6 = participantDTO.getItem6();
-        this.summoner1Id = participantDTO.getSummoner1Id();
-        this.summoner2Id = participantDTO.getSummoner2Id();
+    public PlayerStatsDTO(ParticipantDTO participantDTO, String summoner1Url, String summoner2Url) {
+        this.item0 = RiotConstant.ITEM_IMAGE_URL + participantDTO.getItem0() + ".png";
+        this.item1 = RiotConstant.ITEM_IMAGE_URL + participantDTO.getItem1() + ".png";
+        this.item2 = RiotConstant.ITEM_IMAGE_URL + participantDTO.getItem2() + ".png";
+        this.item3 = RiotConstant.ITEM_IMAGE_URL + participantDTO.getItem3() + ".png";
+        this.item4 = RiotConstant.ITEM_IMAGE_URL + participantDTO.getItem4() + ".png";
+        this.item5 = RiotConstant.ITEM_IMAGE_URL + participantDTO.getItem5() + ".png";
+        this.item6 = RiotConstant.ITEM_IMAGE_URL + participantDTO.getItem6() + ".png";
+        this.summonerSpell1Url = RiotConstant.SUMMONER_SPELL_URL + summoner1Url;
+        this.summonerSpell2Url = RiotConstant.SUMMONER_SPELL_URL + summoner2Url;
         this.deaths = participantDTO.getDeaths();
         this.kills = participantDTO.getKills();
         this.assists = participantDTO.getAssists();
-//        this.kda = calculateKDA(participantDTO.getKills(), participantDTO.getAssists(), participantDTO.getDeaths());
         this.kda = participantDTO.getChallenges().getKda();
         this.pentaKills = participantDTO.getPentaKills();
         this.quadraKills = participantDTO.getQuadraKills();
@@ -102,6 +103,7 @@ public class PlayerStatsDTO {
         this.champLevel = participantDTO.getChampLevel();
         this.championId = participantDTO.getChampionId();
         this.championName = participantDTO.getChampionName();
+        this.championImageUrl = RiotConstant.CHAMPION_IMAGE_URL + participantDTO.getChampionName() + ".png";
         this.teamPosition = participantDTO.getTeamPosition();
         this.magicDamageDealt = participantDTO.getMagicDamageDealt();
         this.magicDamageDealtToChampions = participantDTO.getMagicDamageDealtToChampions();
